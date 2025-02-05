e30d9707bef7cf5bd800605a02ada7b2fd509eab
Mohamed DIAG <Mohamed.DIAG.dz@viacesi.fr>
2025-02-05 12:10:54 +0100

    Add article

--- back/src/schema.ts
+++ back/src/schema.ts
@@ -1,39 +1,55 @@
-import ggl from 'graphql-tag';
+import ggl from "graphql-tag";
 
 export const typeDefs = ggl`
     type Mutation {
-    createUser(username: String!, password: String!): CreateUserResponse
-    signIn(username: String!, password: String!): SignInResponse
+        createUser(username: String!, password: String!): CreateUserResponse
+        signIn(username: String!, password: String!): SignInResponse
+        addArticle(title: String!, content: String!): AddArticleResponse
     }
 
     type Query {
-    me: getUserResponse
+        me: getUserResponse
     }
 
     type User {
-    id: ID!
-    username: String!
+        id: ID!
+        username: String!
+    }
+
+    type Article {
+        id: ID!
+        title: String!
+        content: String!
+        author: User!
+        createdAt: String!
     }
 
     type getUserResponse {
-    code: Int!
-    success: Boolean!
-    message: String!
-    user: User
+        code: Int!
+        success: Boolean!
+        message: String!
+        user: User
     }
- 
+
     type CreateUserResponse {
-    code: Int!
-    success: Boolean!
-    message: String!
-    user: User
+        code: Int!
+        success: Boolean!
+        message: String!
+        user: User
     }
 
     type SignInResponse {
-    code: Int!
-    success: Boolean!
-    message: String!
-    token: String
-    user: User
+        code: Int!
+        success: Boolean!
+        message: String!
+        token: String
+        user: User
+    }
+
+    type AddArticleResponse {
+        code: Int!
+        success: Boolean!
+        message: String!
+        article: Article
     }
 `;
